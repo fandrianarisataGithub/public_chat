7bc88952f2cf4d1f4461fc116816caaaffd6930f
Fenitriniaina <git.fandrianarisata@gmail.com>
2023-09-23 21:38:13 +0300

    send mail, create home page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,8 +52,8 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('_preview_error');
+            
+            return new Response("Un email a été envoyé à votre adresse email");
         }
 
         return $this->render('registration/register.html.twig', [
@@ -78,6 +78,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
